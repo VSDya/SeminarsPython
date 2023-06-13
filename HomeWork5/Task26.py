4789a5bf9b3a7fdcf608b00c63b5ae0c25bb1766
VSDya <vdyadyanin@yandex.ru>
2023-06-14 00:39:41 +0300

    All Seminars Python

--- HomeWork5/Task26.py
+++ HomeWork5/Task26.py
@@ -5,11 +5,6 @@
 # A = 3; B = 5 -> 243 (3⁵)
 # A = 2; B = 3 -> 8
 
-# num_a = int(input("Введите число: "))
-# num_b = int(input("Введите степень числа: "))
-# result = num_a**num_b
-# print(f"A = {num_a}; B = {num_b} -> {result}")
-
 num_a = int(input("Введите число: "))
 num_b = int(input("Введите степень числа: "))
 

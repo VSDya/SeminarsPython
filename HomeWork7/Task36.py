551b37b7c4118020a325c3dd4a54413083865252
VSDya <vdyadyanin@yandex.ru>
2023-06-21 14:27:04 +0300

    All Seminars Python

--- HomeWork7/Task36.py
+++ HomeWork7/Task36.py
@@ -12,10 +12,9 @@
 #                                             5 10 15 20 25 30
 #                                             6 12 18 24 30 36 
 
-def print_operation_table(operation,num_rows,num_columns):   
-    for i in range(1,num_rows+1):
-        print()
-        for j in range(1,num_columns+1):
-            print(operation(i,j), end="   ")
+def print_operation_table(operation, num_rows=6, num_columns=6):
+    a = [[operation(i, j) for j in range(1, num_columns + 1)] for i in range(1, num_rows + 1)]
+    for i in a:
+        print(*[f"{x:>3}" for x in i])
 
-print_operation_table(lambda x, y: x * y,6,6)
+print_operation_table(lambda x, y: x * y)
